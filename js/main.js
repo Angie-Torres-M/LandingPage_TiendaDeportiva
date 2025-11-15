ea1899e5e88e84e18e4b7fbf2df0c60dda4042f7
Angélica Torres <anguietorres.92@gmail.com>
2025-11-14 18:56:52 -0600

    Agrego el footer, header carpeta de imagenes y agrego algunos scripts para la funcioonalidad de los modulos en el main.js el archivo style.css sigue libre. Faltaria un carrucel asi super vistozo de deportes que deseariamos practicar jajaja, seccion de productos esto te lo encargo jared y hasta abjo una seccion de contacto para poner en practica el menu que nos enseño de bootstrap. NO LO HICE RESPONSIVO, puedo hacerlo si quieren pero no viene en la rúbrica jeje

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Incluir fragmentos HTML
+async function includeHTML(selector, url) {
+  const host = document.querySelector(selector);
+  if (!host) return; // si no existe el contenedor, salimos
+
+  const res = await fetch(url, { cache: "no-cache" });
+  if (!res.ok) {
+    console.error(`No se pudo cargar ${url}`);
+    return;
+  }
+
+  host.innerHTML = await res.text();
+}
+// Ajustar margen del <main>
+
+function adjustMainOffset() {
+  // header viene de header.html dentro de #site-header
+  const header = document.querySelector("#site-header header");
+  // main está en tu index.html
+  const main = document.querySelector("main");
+
+  if (header && main) {
+    const h = header.offsetHeight;
+    main.style.marginTop = `${h}px`;
+  }
+}
+// Inicializar
+document.addEventListener("DOMContentLoaded", async () => {
+  // 1) Inserta HEADER
+  await includeHTML("#site-header", "./header.html");
+
+  // 2) Inserta FOOTER
+  await includeHTML("#site-footer", "./footer.html");
+
+  // 3) Ajusta margen del main (ya existe el header)
+  adjustMainOffset();
+
+  // 4) Recalcular en resize por si cambia la altura
+  window.addEventListener("resize", adjustMainOffset);
+});
